fd5e672a0fbd686dd37a1119e89731c9e79cf65a
joaquin <joaquin@joaquin-Notebook>
2016-01-09 15:47:43 +0000

    Revert "Kata 3 version final"
    This reverts commit 10fc2a6051c948def669265d9719b880bde4667e.

--- src/kata3/Main.java
+++ src/kata3/Main.java
@@ -1,24 +1,42 @@
 package kata3;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-
 public class Main {
 
     public static void main(String[] args) {
-        Histogram<String> histogram = new Histogram<>();
-        String fileName= "/home/joaquin/NetBeansProjects/KATA3/Data/emailsfilev1.txt";
-        try {
-            BufferedReader filenIn = new BufferedReader(new FileReader(fileName));
-            String mail;
-            while((mail=filenIn.readLine())!=null){
-                if(mail.contains("@")){
-                    histogram.increment(mail.split("@")[1]);
-                }
-            }
-        } catch (Exception e) {
-            System.out.println("El fichero no existe");
-        }
+        Histogram<String> histogram = new Histogram<>(); 
+        histogram.increment("gmail.com");
+        histogram.increment("gmail.com");
+        histogram.increment("gmail.com");
+        histogram.increment("gmail.com");
+        histogram.increment("gmail.com");
+        histogram.increment("gmail.com");
+        histogram.increment("gmail.com");
+        histogram.increment("gmail.com");
+        histogram.increment("gmail.com");
+        
+        histogram.increment("ulpgc.es");
+        histogram.increment("ulpgc.es");
+        histogram.increment("ulpgc.es");
+        histogram.increment("ulpgc.es");
+        histogram.increment("ulpgc.es");
+        histogram.increment("hotmail.es");
+        histogram.increment("hotmail.es");
+        histogram.increment("hotmail.es");
+        histogram.increment("hotmail.es");
+        histogram.increment("hotmail.es");
+        histogram.increment("hotmail.es");
+        histogram.increment("hotmail.es");
+        histogram.increment("hotmail.es");
+        histogram.increment("hotmail.es");
+        histogram.increment("hotmail.es");
+        histogram.increment("hotmail.es");
+        histogram.increment("hotmail.es");
+        histogram.increment("hotmail.es");
+        histogram.increment("hotmail.es");
+        histogram.increment("hotmail.es");
+        histogram.increment("hotmail.es");
+        histogram.increment("hotmail.es");
+        histogram.increment("hotmail.es");
         HistogramDisplay histogramDisplay = new HistogramDisplay(histogram);
         histogramDisplay.execute();
     }
--- src/kata3/getDominio.java
+++
@@ -1,5 +0,0 @@
-package kata3;
-
-public class getDominio {
-    //implementar aqui la parte que esta en main que lee los dominios del fichero
-}
